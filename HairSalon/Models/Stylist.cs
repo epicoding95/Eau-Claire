37d8d40a5d90a6bc93800314d0cfd57ccad01c65
Student <example@example.com>
2019-08-09 17:37:03 -0700

    weird error

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,10 +4,10 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
-        public int stylistId {get; set;}
-        public string Name {get; set;}
-        public string Details {get; set;}
+        public int stylistId { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
 
-        public virtual Client client {get; set;}
+        public virtual Client client { get; set; }
     }
 }
